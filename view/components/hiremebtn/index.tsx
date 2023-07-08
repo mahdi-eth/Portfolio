87c1275329a99ab25719fb6fc4771167759a2671
Mahdi-Ettehadnejad <mahdi.ettehad85@gmail.com>
2023-07-08 10:46:32 +0330

    Added multiple pages

--- view/components/hiremebtn/index.tsx
+++ view/components/hiremebtn/index.tsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
+
 const HireMe: React.FC = () => {
     return (
-        <button to="connect" smooth={true} duration={800} className="w-30 min-w-max py-2 text-_black font-semibold hover:opacity-90 bg-_orange px-8 rounded-full">
+        <Link
+            href="/connect"
+            className="w-30 min-w-max py-2 text-_black font-semibold hover:opacity-90 bg-_orange px-8 rounded-full">
             Hire Me
-        </button>
+        </Link>
     );
 };
 
